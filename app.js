d5f4a015bce7aa694d5c0aab6ebbfab596cdeaa5
jonathanmiz08 <jonathanmizhirumbay@outlook.com>
2024-10-28 12:07:04 -0400

    In process of adding gallery

--- app.js
+++ app.js
@@ -1,6 +1,34 @@
-const menu = document.querySelector('#mobile-menu')
+/*const menu = document.querySelector('#mobile-menu')
 const menuLinks = document.querySelector('.navbar_menu')
 menu.addEventListener('click', function () {
     menu.classList.toggle('is-active')
     menuLinks.classList.toggle('active');
 })
+
+ */
+
+import Masonry from 'masonry-layout';
+
+/*
+window.onload = () => {
+    const grid = document.querySelector('.grid');
+    const masonry = new Masonry(grid, {
+        itemSelector: '.grid-item',
+        gutter: 10,
+        originLeft: false,
+        originTop: false,
+    });
+    masonry.on('layoutComplete', () => console.log('Layout Complete'));
+};
+
+ */
+import Masonry from 'masonry-layout';
+
+const $grid = $('.grid').imagesLoaded(() => {
+    $('.grid').masonry({
+        itemSelector: '.grid-item',
+        columnWidth: '.grid-item',
+        fitWidth: true,
+    })
+});
+
